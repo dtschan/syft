b860f47fc48eb4ffa23853c7ee01d2f4b7b88d44
Daniel Tschan <tschan@puzzle.ch>
2021-12-19 20:00:54 +0100

    Exclude components owned by packages

--- internal/formats/common/cyclonedxhelpers/format.go
+++ internal/formats/common/cyclonedxhelpers/format.go
@@ -1,11 +1,14 @@
 package cyclonedxhelpers
 
 import (
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/CycloneDX/cyclonedx-go"
 	"github.com/anchore/syft/internal"
 	"github.com/anchore/syft/internal/version"
+	"github.com/anchore/syft/syft/artifact"
 	"github.com/anchore/syft/syft/pkg"
 	"github.com/anchore/syft/syft/sbom"
 	"github.com/anchore/syft/syft/source"
@@ -22,10 +25,27 @@ func ToFormatModel(s sbom.SBOM) *cyclonedx.BOM {
 	cdxBOM.SerialNumber = uuid.New().URN()
 	cdxBOM.Metadata = toBomDescriptor(internal.ApplicationName, versionInfo.Version, s.Source)
 
+	ownedPackages := map[artifact.ID]artifact.ID{}
+	for _, r := range s.Relationships {
+		if r.Type == artifact.OwnershipByFileOverlapRelationship {
+			ownedPackages[r.To.ID()] = r.From.ID()
+		}
+	}
+
 	packages := s.Artifacts.PackageCatalog.Sorted()
-	components := make([]cyclonedx.Component, len(packages))
-	for i, p := range packages {
-		components[i] = toComponent(p)
+	components := make([]cyclonedx.Component, 0, len(packages))
+	for _, p := range packages {
+		if owner, ok := ownedPackages[p.ID()]; ok {
+			fmt.Fprintf(os.Stderr, "Skipping %s, is owned by %s\n", p.ID(), owner)
+		} else {
+			components = append(components, toComponent(p))
+		}
+
+		// s.Artifacts.PackageCatalog.
+		// fmt.Fprintln(os.Stderr, s.Relationships)
+		//fmt.Fprintln(os.Stderr, p.Name, p.Locations[0].RealPath, s.Artifacts.PackageCatalog.PackagesByPath(p.Locations[0].RealPath))
+
+		// fmt.Fprintln(os.Stderr, p.Name, p.Locations)
 	}
 	cdxBOM.Components = &components
 
